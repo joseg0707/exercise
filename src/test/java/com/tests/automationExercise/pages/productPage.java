3c4fc2416f3e3059bca14532c2f5ca9f5bd55523
joseg0707 <123908823+joseg0707@users.noreply.github.com>
2023-06-22 00:48:07 -0500

    6/22/23

--- src/test/java/com/tests/automationExercise/pages/productPage.java
+++ src/test/java/com/tests/automationExercise/pages/productPage.java
@@ -2,6 +2,7 @@ package com.tests.automationExercise.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.BrowserUtils;
@@ -29,7 +30,11 @@ public class productPage {
     }
 
     public void viewProduct() throws InterruptedException {
-        BrowserUtils.scrollIntoView(blueTop);
+
+        Thread.sleep(3000);
+        Actions actions = new Actions(driver);
+        actions.moveToElement(blueTop);
+        actions.perform();
 
         blueTop.click();
 
